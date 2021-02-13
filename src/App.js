2829b02a8943797d689d94c8df5da252576da764
Juan Quispe <jquispeh63@gmail.com>
2021-02-12 20:16:13 -0500

    Adds previously finished tasks
    -Adds function to add conditions (Previous tasks) (Stub)
    -Adds previous tasks to Task and shows them

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Task from './Task'
 
 function App() {
   const [taskName, setTaskName] = useState("");
+  const [taskPrevs, setTaskPrevs] = useState(new Array());
   const [tasks, setTasks] = useState(new Array());
 
   function createTask(){
     const newTask = {
-      name: taskName
+      name: taskName,
+      prevs: taskPrevs
     };
 
     setTasks([...tasks, newTask])
+    setTaskPrevs(new Array());
     setTaskName("")
+  }
+
+  function createPrev(){
+    let newPrev = parseInt(prompt("Enter the number of the task", "1"));
+    setTaskPrevs([...taskPrevs, newPrev])
   }
 
   function deleteTask(index){
@@ -24,14 +32,14 @@ function App() {
     <>
       <div id="tasks">
         {tasks.map((task, index) => {
-          return <Task key={index} name={task.name} index={index} onClick={deleteTask}/>
+          return <Task key={index} name={task.name} index={index} prevs={task.prevs} onClick={deleteTask}/>
         })}
       </div>
       <div id="editor">
         <textarea placeholder="Insert name of a task here..." onChange={evt => setTaskName(evt.target.value)} value={taskName}></textarea>
         <div>
           <button onClick={createTask} disabled={taskName.trim().length == 0}>Create Task</button>
-          <button>Add Previous Tasks</button>
+          <button onClick={createPrev}>Add Previous Tasks</button>
         </div>
       </div>
     </>
--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,10 @@
 const Task = params => {
     const name = params.name;
     const index = params.index;
+    const prevs = params.prevs;
     return (
         <div className="task">
-            <span className="name-task">{`${index + 1} - ${name}`}</span>
+            <span className="name-task">{`${index + 1} - ${name} [Requires task(s): ${(prevs.length > 0) ? prevs.join(",") : "None"}]`}</span>
             <div>
                 <input type="checkbox" className="check-task"></input>
                 <button className="delete-task" onClick={() => params.onClick(index)}>✖</button>
